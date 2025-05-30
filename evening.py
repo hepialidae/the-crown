ce72a0533d89e3c3931153a6a9e6cc4583d1fd18
luminousseelie <luminousseelie@gmail.com>
2025-05-30 18:24:43 +0800

    added silly article

--- evening.py
+++ evening.py
@@ -1,8 +1,12 @@
 import gamestate_tracking
 from tutorial import sleep
+from newspaper import Newspaper
 from may_change import secretary_title
 
 def evening():
+    # newspaper stuff
+    gamestate_tracking.todays_newspaper.add_silly_article()
+    
     print(f"\n{secretary_title}: Good evening, {gamestate_tracking.player_name}.")
     sleep()
     print(f"{secretary_title}: {gamestate_tracking.newspaper_outlet} has sent over their newspaper for you to review.")
