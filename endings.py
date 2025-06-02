8052a4d1c535e270940b38341e42bbb1a6f12518
hepialidae <luminousseelie@gmail.com>
2025-06-02 10:57:19 +0800

    small edit

--- endings.py
+++ endings.py
@@ -4,7 +4,6 @@ from tutorial import sleep
 
 def check_endings():
     if faction_dict["The Commoners"].favor <= 20 and faction_dict["The Iron Guard"].presence <= 20: # commoner riot ending
-        gamestate_tracking.phase = 4
         print(f"\n<ENDING 1>")
         sleep(0.8)
         print("<The Commoners are unhappy with their treatment.>")
@@ -12,4 +11,14 @@ def check_endings():
         print("<They riot, and the Iron Guard is unable to stop them.>")
         sleep()
         print("<The city is in chaos, and you are executed by the mob.>\n")
-    
+        gamestate_tracking.phase = 4
+
+
+# ENDING PLANNING:
+# 6 main endings, lean into one faction/balance -> what if the player leans into two factions?
+# 3 phases for each ending, tracked in gamestate_tracking.py
+# Academocracy/meritocracy: 
+# Theocracy:
+# Communist dictatorship:
+# Authoritarian dictatorship: 
+# Plutocracy:
