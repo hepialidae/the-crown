2c2ba84754d9b5fb2798e0bb21977e9affee814c
hepialidae <luminousseelie@gmail.com>
2025-06-02 10:51:46 +0800

    content edits :3

--- decision_names.py
+++ decision_names.py
@@ -1,6 +1,6 @@
 decision_names = [
     [
-        "The Arcanum asks to be permitted to research battle magic.\nOption A +10 to Arcanum and -10 to Veiled; Option B -10 to Arcanum.",
+        "The Arcanum asks to be permitted to research battle magic.\nOption A: Give them the permit. (+10 to Arcanum and -10 to Veiled)\nOption B: Reject them. (-10 to Arcanum)",
         True,
         [
             ["The Arcanum", "favor", 10], 
@@ -12,7 +12,7 @@ decision_names = [
         "battle magic research permit"
     ],
     [
-        "The Commander of the Iron Guard requests more military bases around the city.\nOption A +10 presence to Iron Guard and +5 suspicion to all other factions; Option B -10 favor to Iron Guard.",
+        "The Commander of the Iron Guard requests more military bases around the city.\nOption A: Build military bases. (+10 presence to Iron Guard and +5 suspicion to all other factions)\nOption B: Reject them. (-10 favor to Iron Guard)",
         True,
         [
             ["The Iron Guard", "presence", 10], 
@@ -27,7 +27,7 @@ decision_names = [
         "military bases"
     ],
     [
-        "A merchant from the Gilded wants to screw over Commoners.\nOption A +10 favor to Gilded and -10 favor to Commoners; Option B -10 favor to Gilded and +10 favor to Commoners.",
+        "A merchant from the Gilded wants to screw over Commoners.\nOption A: Turn a blind eye. (+10 favor to Gilded and -10 favor to Commoners)\nOption B: Stop them. (-10 favor to Gilded and +10 favor to Commoners)",
         True,
         [
             ["The Gilded", "favor", 10], 
@@ -40,7 +40,7 @@ decision_names = [
         "Commoner scams"
     ],
     [
-        "The Commoners want to build shelters for the homeless in place of a new military base.\nOption A +10 favor to Commoners and -10 presence to Iron Guard; Option B -10 favor to Commoners.",
+        "The Commoners want to build shelters for the homeless in place of a new military base.\nOption A: Reject plans for military base and build homeless shelter. (+10 favor to Commoners and -10 presence to Iron Guard)\nOption B: Reject the homeless. (-10 favor to Commoners)",
         True,
         [
             ["The Commoners", "favor", 10], 
